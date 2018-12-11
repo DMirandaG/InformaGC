587231d96ab2831729f3ed136aaea4418d725a99
DavidMG2 <david_smg_92@hotmail.com>
2018-12-11 17:19:43 +0000

    Añadido menú

--- app/src/main/java/pem/informagc/vista/VistaPasoUno.java
+++ app/src/main/java/pem/informagc/vista/VistaPasoUno.java
@@ -1,14 +1,22 @@
 package pem.informagc.vista;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import pem.informagc.AppMediador;
 import pem.informagc.R;
 
-public class VistaPasoUno extends AppCompatActivity implements IVistaPasoUno {
+public class VistaPasoUno extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, IVistaPasoUno {
 
 
     private CardView cardAceras;
@@ -22,6 +30,8 @@ public class VistaPasoUno extends AppCompatActivity implements IVistaPasoUno {
     private CardView cardSemaforos;
     private CardView cardParques;
     private CardView cardSaneamiento;
+    private AppMediador appMediador;
+    private DrawerLayout drawer;
 
 
 
@@ -107,6 +117,21 @@ public class VistaPasoUno extends AppCompatActivity implements IVistaPasoUno {
                 irAPasoDos();
             }
         });
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
 
@@ -114,5 +139,38 @@ public class VistaPasoUno extends AppCompatActivity implements IVistaPasoUno {
     public void irAPasoDos() {
         Intent intent = new Intent(this, VistaPasoDos.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_perfil) {
+            Toast.makeText(this,"Perfil",Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.nav_mis_informes) {
+            Toast.makeText(this,"Mis Informes",Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.nav_como_funciona) {
+            Toast.makeText(this,"Como Funciona",Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.nav_enviar_sugerencias) {
+            Toast.makeText(this,"Enviar Sugerencias",Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.nav_terminos) {
+            Toast.makeText(this,"Terminos y Condiciones",Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.nav_cerrar_sesion) {
+            Toast.makeText(this,"Cerrar Sesion",Toast.LENGTH_SHORT).show();
+        }
+        drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
