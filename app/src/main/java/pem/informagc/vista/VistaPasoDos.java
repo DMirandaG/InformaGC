596010405b9453f4c5249edf6a7a9ffa084a7e4e
DavidMG2 <david_smg_92@hotmail.com>
2018-12-11 20:26:00 +0000

    Paso 2 añadido

--- app/src/main/java/pem/informagc/vista/VistaPasoDos.java
+++ app/src/main/java/pem/informagc/vista/VistaPasoDos.java
@@ -1,15 +1,69 @@
 package pem.informagc.vista;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import pem.informagc.R;
 
-public class VistaPasoDos extends AppCompatActivity implements IVistaPasoDos {
+public class VistaPasoDos extends AppCompatActivity implements IVistaPasoDos, OnMapReadyCallback {
+
+
+    private Button botonEnviar;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         setContentView(R.layout.activity_vista_paso_dos);
+
+        botonEnviar = (Button) findViewById(R.id.botonEnviar);
+        botonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Está seguro que desea enviar su Informe?")
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        irAPasoUno();
+                    }
+                }).setNegativeButton("No",null);
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    @Override
+    public void irAPasoUno() {
+        Intent intent = new Intent(this, VistaPasoUno.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 }
